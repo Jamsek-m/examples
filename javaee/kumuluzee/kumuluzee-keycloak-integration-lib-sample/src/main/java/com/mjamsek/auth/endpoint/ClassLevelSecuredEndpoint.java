18d41a324d9088b6170231aebe87df93c6e074f3
Miha Jamsek <miha_jamsek@windowslive.com>
2020-03-23 15:51:51 +0100

    updated sample

--- javaee/kumuluzee/kumuluzee-keycloak-integration-lib-sample/src/main/java/com/mjamsek/auth/endpoint/ClassLevelSecuredEndpoint.java
+++ javaee/kumuluzee/kumuluzee-keycloak-integration-lib-sample/src/main/java/com/mjamsek/auth/endpoint/ClassLevelSecuredEndpoint.java
@@ -5,6 +5,7 @@ import com.mjamsek.auth.keycloak.annotations.PublicResource;
 import com.mjamsek.auth.keycloak.annotations.SecureResource;
 
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import javax.ws.rs.core.Response;
 @RequestScoped
 @Path("/class-level-secure")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ClassLevelSecuredEndpoint {
     
     @GET
@@ -26,6 +28,7 @@ public class ClassLevelSecuredEndpoint {
     }
     
     @GET
+    @Path("/public")
     @PublicResource
     public Response publicMethod() {
         String response = "{\"message\": \"This is public endpoint!\"}";
